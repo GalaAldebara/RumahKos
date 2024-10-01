9c6c91a1bcd5a023e23ab99e35198dcb9191cca0
Yusriyah Firjatullah <57206360+yusriyahf@users.noreply.github.com>
2024-10-01 16:47:42 +0700

    Membuat Front end login + dashboard

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>RumahKos</title>
+  <link rel="shortcut icon" type="image/png" href="images/logos/favicon.png" />
+  <link rel="stylesheet" href="css/styles.min.css" />
+</head>
+
+<body>
+  <!--  Body Wrapper -->
+  <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+    data-sidebar-position="fixed" data-header-position="fixed">
+    <!-- Sidebar Start -->
+    <aside class="left-sidebar">
+      <!-- Sidebar scroll-->
+      <div>
+        <div class="brand-logo d-flex align-items-center justify-content-between">
+          <a href="./index.html" class="text-nowrap logo-img">
+            <img src="images/logos/dark-logo.svg" width="180" alt="" />
+          </a>
+          <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+            <i class="ti ti-x fs-8"></i>
+          </div>
+        </div>
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+         @include('layouts.sidebar')
+        </nav>
+        <!-- End Sidebar navigation -->
+      </div>
+      <!-- End Sidebar scroll-->
+    </aside>
+    <!--  Sidebar End -->
+    <!--  Main wrapper -->
+    <div class="body-wrapper">
+      <!--  Header Start -->
+      <header class="app-header">
+       @include('layouts.navbar')
+      </header>
+      <!--  Header End -->
+      <div class="container-fluid">
+        
+        @yield('container')
+        
+        @include('layouts.footer')
+      </div>
+    </div>
+  </div>
+  <script src="libs/jquery/dist/jquery.min.js"></script>
+  <script src="libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="js/sidebarmenu.js"></script>
+  <script src="js/app.min.js"></script>
+  <script src="libs/apexcharts/dist/apexcharts.min.js"></script>
+  <script src="libs/simplebar/dist/simplebar.js"></script>
+  <script src="js/dashboard.js"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login/index');
+});
+
+Route::get('/register', function () {
+    return view('login/register');
+});
+Route::get('/dashboard', function () {
+    return view('dashboard/index');
+});
+Route::get('/contract', function () {
+    return view('contract/index');
+});
+
+
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
+
+
+Route::get('/income', function () {
+    return view('income/index');
 });
