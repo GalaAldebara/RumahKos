d2723dfcedbc72b8f4bcd5272b6b312a5eb23796
GalaAldebara <lalasipo20@gmail.com>
2024-10-08 18:52:52 +0700

    Login Page

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 
+Route::get('/nyobalogin', function () {
+    return view('login/index2');
+});
 
 Route::get('/', function () {
     return view('login/index');
